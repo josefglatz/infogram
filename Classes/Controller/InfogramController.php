e0f6b9df23754e390b9ff686025a8c4b3f3acf25
Josef Glatz <jousch@gmail.com>
2017-08-23 07:06:52 +0200

    [CGL] Controller
    Related: #15

--- Classes/Controller/InfogramController.php
+++ Classes/Controller/InfogramController.php
@@ -9,6 +9,10 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
 
+/**
+ * Class InfogramController
+ * @package JosefGlatz\Infogram\Controller
+ */
 class InfogramController extends ActionController
 {
     /**
@@ -21,12 +25,20 @@ class InfogramController extends ActionController
      */
     protected $extensionConfiguration;
 
+    /**
+     * Tasks which all actions have in common: instanciate ApiService and extConf
+     */
     public function initializeAction()
     {
         $this->infogramApi = GeneralUtility::makeInstance(ApiService::class);
         $this->extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
     }
 
+    /**
+     * Assign variables common for all actions
+     *
+     * @param ViewInterface $view
+     */
     protected function initializeView(ViewInterface $view)
     {
         $view->assign('contentObjectData', $this->configurationManager->getContentObject()->data);
@@ -34,7 +46,11 @@ class InfogramController extends ActionController
         parent::initializeView($view);
     }
 
-    public function showAction() {
+    /**
+     * Single view of an infographic
+     */
+    public function showAction()
+    {
         if (!empty($this->settings['infographicId'])) {
             $this->view->assign(
                 'infographic',
