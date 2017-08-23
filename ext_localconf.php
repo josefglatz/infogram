2133cb510f9990b95f6f7f21a7d002cd9ce0fd50
Josef Glatz <jousch@gmail.com>
2017-08-23 07:35:32 +0200

    [TASK] Rename PageTSConfig file extension
    Related: #15

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function ($extKey) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TSConfig/ContentElementWizard.txt">');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TSConfig/ContentElementWizard.typoscript">');
 
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
             'JosefGlatz.' . $extKey,
